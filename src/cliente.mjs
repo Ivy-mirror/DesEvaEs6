2a1354dceaa96bb525c70c4816d2c71832c13c87
ivania <ivaniaespejoduran@gmail.com>
2024-08-06 17:03:45 -0400

    My first commit

---
+++ src/cliente.mjs
@@ -0,0 +1,26 @@
+
+class Cliente {
+  constructor(cliente) {
+    this.nombre = nombre
+    this.impuesto = impuesto
+  }
+
+  
+  get nombre() {
+    return this.nombre
+  }
+
+  set nombre(nuevoNombre) {
+    this.nombre.push(nuevoNombre)
+  }
+
+  calcularImpuestos() {
+    return this.impuesto
+  }
+}
+
+
+let Cliente = new Cliente()
+
+
+
---
+++ src/main.mjs
@@ -0,0 +1,4 @@
+
+import {Cliente} from "./cliente.mjs";
+import {Impuestos} from "./impuestos.mjs";
+
